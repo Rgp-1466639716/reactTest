2151dc3a0d1ca5e221a2e0bfb080cd3175d50b34
rgp <rgp1466639716@126.com>
2024-07-14 22:39:18 +0800

    sgc-less sass less得配置

--- src/page/Layout/index.js
+++ src/page/Layout/index.js
@@ -1,9 +1,16 @@
 // import { useSearchParams,useParams } from 'react-router-dom'
-import { Button } from 'antd-mobile'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import { getBillList } from "@/store/modules/billStore"
+import React, { useState } from 'react'
+import { TabBar } from 'antd-mobile'
+import {
+  AppOutline,
+  MessageFill,
+  UnorderedListOutline,
+} from 'antd-mobile-icons'
+import './index.scss'
 
 const Layout =()=>{
   const dispatch = useDispatch()
@@ -11,13 +18,40 @@ const Layout =()=>{
     dispatch(getBillList())
   },[dispatch])
   const { billList } = useSelector(state=>state.bill)
-  console.log(111111,billList)
-  return(
-    <div>
-      我是一级路由
-      <Button color='primary'>11111</Button>
-      {/* 二级路由显示位置 */}
-      <Outlet/>
+  const tabs = [
+    {
+      key: 'Month',
+      title: '月度账单',
+      icon: <AppOutline />,
+    },
+    {
+      key: 'New',
+      title: '待办',
+      icon: <UnorderedListOutline />,
+    },
+    {
+      key: 'Year',
+      title: '年度账单',
+      icon: <MessageFill /> ,
+    },
+  ]
+  const navigate = useNavigate()
+  const switchRoute = (path)=>{
+    navigate(path)
+  }
+  return (
+    <div className='layout'>
+      <div className='container'>
+        二级路由占位
+        <Outlet/>
+      </div>
+      <div className='footer'>
+        <TabBar onChange={switchRoute}>
+          {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
     </div>
   )
 }
--- src/page/Month/index.js
+++ src/page/Month/index.js
@@ -1,6 +1,29 @@
 // import { useSearchParams,useParams } from 'react-router-dom'
 
+import { useSelector } from "react-redux"
+
 const Month =()=>{
-  return<div>Month</div>
+  const {billList }= useSelector(state=>state.bill)
+  console.log(1111232312,billList)
+  const group = {}
+  billList.forEach(item => {
+    const key = item.type
+    if(!group[key]){
+      group[key]=[]
+    }
+    group[key].push(item)
+  });
+  console.log(1232,group)
+  const adc = billList.reduce((pre,cur)=>{
+    const key = cur.type
+    if(!pre[key]){
+      pre[key]=[]
+    }
+    pre[key].push(cur)
+    return pre/
+  },{})
+  console.log(123321,adc)
+  const ab = true
+  return<div className={`${ab && "sss"} aaa`}>Month</div>
 }
 export default Month
