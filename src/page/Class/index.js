0bc691ea1033ef3906618281ecf3d321fe856f21
rgp <rgp1466639716@126.com>
2024-07-27 22:24:50 +0800

    生命周期实验

---
+++ src/page/Class/index.js
@@ -0,0 +1,64 @@
+import { Button } from "antd-mobile";
+import { Component, useState } from "react";
+
+class Son extends Component {
+  constructor(props){
+    // 初始化state
+    console.log(111,'consructor')
+    super(props)
+    this.state ={
+      count: 0
+    }
+  }
+  setCount = ()=>{
+    this.setState({
+      count:1
+    })
+  }
+  static getDerivedStateFromProps(nextProps,nextState){
+    // 用于props变化更新state
+    console.log(111,'getDerivedStateFromProps')
+    return {
+      count:nextProps.title
+    }
+  }
+  shouldComponentUpdate(nextProps,nextState){
+    // 组件是否更新 true更新
+    console.log(111,'shouldComponentUpdate',nextProps,nextState)
+    return true
+  }
+  getSnapshotBeforeUpdate(preProps, preState) {
+    // 获取更新前的数据，存储更新前的先前值，跟底下钩子一起使用
+    console.log(111,'getSnapshotBeforeUpdate',preProps,preState)
+    return 11111
+  }
+  componentDidUpdate(preProps, preState, snapshot){
+    // 组件更新完调用 snapshot是上边生命周期返回的值
+    console.log(111,'componentDidUpdate',preProps,preState,snapshot)
+  }
+  componentDidMount(){
+    // 组件挂载完毕时自动执行,执行副作用，调后端接口等
+    console.log(111,'conponentDidMount')
+  }
+  componentWillUnmount(){
+    // 组件卸载时执行，清除定时器，副作用等
+    console.log(111,'componentWillUnmount')
+  }
+  render(){
+    // 创建或更新 vdom
+    console.log('111','render',this.state)
+    const { count } = this.state
+    return <div onClick={this.setCount}>{count}</div>
+  }
+}
+function Counter () {
+  const [ show, setShow ] = useState(true)
+  const [ title, setTitle ] = useState(1)
+  return (
+    <div onClick={()=>setShow(true)}>
+      {show && <Son title={title}/>}
+      <Button onClick={()=>{setTitle(3)}}>title</Button>
+    </div>
+  )
+}
+export default Counter
--- src/page/Month/index.js
+++ src/page/Month/index.js
@@ -1,10 +1,10 @@
 // import { useSearchParams,useParams } from 'react-router-dom'
 
 import { useSelector } from "react-redux"
+import Counter from "../Class"
 
 const Month =()=>{
   const {billList }= useSelector(state=>state.bill)
-  console.log(1111232312,billList)
   const group = {}
   billList.forEach(item => {
     const key = item.type
@@ -13,7 +13,6 @@ const Month =()=>{
     }
     group[key].push(item)
   });
-  console.log(1232,group)
   const adc = billList.reduce((pre,cur)=>{
     const key = cur.type
     if(!pre[key]){
@@ -22,8 +21,9 @@ const Month =()=>{
     pre[key].push(cur)
     return pre
   },{})
-  console.log(123321,adc)
   const ab = true
-  return<div className={`${ab && "sss"} aaa`}>Month</div>
+  return<div className={`${ab && "sss"} aaa`}>
+    Month<Counter/>
+    </div>
 }
 export default Month
